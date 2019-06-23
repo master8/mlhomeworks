35b83fda586d7a618c056906d5056761b63f1411
master8 <master8.michael@gmail.com>
2019-06-23 13:16:28 +0500

    page rank

--- rank.py
+++ rank.py
@@ -60,3 +60,49 @@ for i in range(0, len(files_name)):
 
 row_sum = matrix.sum(axis=1)
 matrix_prob = matrix / row_sum[:, np.newaxis]
+
+vector = np.array([0.2]*len(files_name))
+
+for i in range(100):
+    prev_vector = vector
+    vector = matrix_prob.transpose().dot(vector)
+    # print(str(i) + ' ' + str((np.abs(prev_vector - vector).sum())))
+
+
+rank = {}
+
+for i, value in enumerate(vector):
+    rank[i] = value
+
+
+# all
+print()
+print("all")
+print()
+
+new_rank = rank.copy()
+
+items = sorted(new_rank.items(), key=lambda t: t[1])[::-1][:n]
+for index, value in items:
+    print(files_name[index] + ' ' + str(value))
+
+# армстронг
+word = "армстронг"
+
+print()
+print(word)
+print()
+
+page_with_word = []
+
+for i, name in enumerate(files_name):
+    if word.lower() in name.lower():
+        page_with_word.append(i)
+
+new_rank = {}
+for i in page_with_word:
+    new_rank[i] = rank[i]
+
+items = sorted(new_rank.items(), key=lambda t: t[1])[::-1][:n]
+for index, value in items:
+    print(files_name[index] + ' ' + str(value))
